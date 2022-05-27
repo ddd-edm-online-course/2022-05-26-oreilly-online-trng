3fe1e5177c506f16f2bc1bcb0becf4cf78d97218
Matt Stine <matt.stine@gmail.com>
2022-05-27 13:36:28 -0500

    [LAB 5] Submission

--- src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/InProcessEventSourcedPizzaRepository.java
+++ src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/InProcessEventSourcedPizzaRepository.java
@@ -3,6 +3,7 @@ package com.mattstine.dddworkshop.pizzashop.kitchen;
 import com.mattstine.dddworkshop.pizzashop.infrastructure.events.ports.EventLog;
 import com.mattstine.dddworkshop.pizzashop.infrastructure.events.ports.Topic;
 import com.mattstine.dddworkshop.pizzashop.infrastructure.repository.adapters.InProcessEventSourcedRepository;
+import com.mattstine.dddworkshop.pizzashop.ordering.OnlineOrderRef;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -12,12 +13,25 @@ import java.util.stream.Collectors;
 
 final class InProcessEventSourcedPizzaRepository extends InProcessEventSourcedRepository<PizzaRef, Pizza, Pizza.PizzaState, PizzaEvent, PizzaAddedEvent> implements PizzaRepository {
 
+    Map<KitchenOrderRef, Set<PizzaRef>> kitchenOrderRefSetMap = new HashMap<>();
+
     InProcessEventSourcedPizzaRepository(EventLog eventLog, Topic pizzas) {
         super(eventLog, PizzaRef.class, Pizza.class, Pizza.PizzaState.class, PizzaAddedEvent.class, pizzas);
+
+        eventLog.subscribe(pizzas, e -> {
+            if (e instanceof PizzaAddedEvent) {
+                PizzaAddedEvent pae = (PizzaAddedEvent) e;
+                Set<PizzaRef> pizzaRefSet = kitchenOrderRefSetMap.computeIfAbsent(pae.getState().getKitchenOrderRef(), k -> new HashSet<>());
+                pizzaRefSet.add(pae.getRef());
+            }
+        });
     }
 
     @Override
     public Set<Pizza> findPizzasByKitchenOrderRef(KitchenOrderRef kitchenOrderRef) {
-        return null;
+        return kitchenOrderRefSetMap.get(kitchenOrderRef)
+                .stream()
+                .map(this::findByRef)
+                .collect(Collectors.toSet());
     }
 }
